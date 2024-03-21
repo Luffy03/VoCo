a82994b385768283e5de7c9cded9741a74cc368d
Linshan <52514670+Luffy03@users.noreply.github.com>
2024-03-21 18:25:46 +0800

    Update main.py

--- Finetune/BTCV/main.py
+++ Finetune/BTCV/main.py
@@ -65,7 +65,7 @@ roi = 96
 parser.add_argument("--save_checkpoint", default=True, help="save checkpoint during training")
 parser.add_argument("--max_epochs", default=3000, type=int, help="max number of training epochs")
 parser.add_argument("--batch_size", default=1, type=int, help="number of batch size")
-parser.add_argument("--sw_batch_size", default=4, type=int, help="number of sliding window batch size")
+parser.add_argument("--sw_batch_size", default=16, type=int, help="number of sliding window batch size")
 parser.add_argument("--optim_lr", default=3e-4, type=float, help="optimization learning rate")
 parser.add_argument("--optim_name", default="adamw", type=str, help="optimization algorithm")
 parser.add_argument("--reg_weight", default=1e-5, type=float, help="regularization weight")
