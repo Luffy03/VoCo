6f0e9d00ab66cedefca4e707d09e6c0afaf13364
Linshan <52514670+Luffy03@users.noreply.github.com>
2024-04-13 09:26:36 +0800

    Add files via upload

---
+++ Finetune/nnUNet/nnunetv2/training/nnUNetTrainer/nnUNetTrainer_swin.py
@@ -0,0 +1,157 @@
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
+import torch
+from dynamic_network_architectures.architectures.unet import ResidualEncoderUNet, PlainConvUNet
+from dynamic_network_architectures.building_blocks.helper import convert_dim_to_conv_op, get_matching_batchnorm
+from dynamic_network_architectures.initialization.weight_init import init_last_bn_before_add_to_0, InitWeights_He
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
+from nnunetv2.utilities.plans_handling.plans_handler import ConfigurationManager, PlansManager
+from torch import nn
+from nnunetv2.training.nnUNetTrainer.vit import Swin
+
+
+class nnUNetTrainer_swin(nnUNetTrainer):
+    def __init__(
+        self,
+        plans: dict,
+        configuration: str,
+        fold: int,
+        dataset_json: dict,
+        unpack_dataset: bool = True,
+        device: torch.device = torch.device("cuda"),
+    ):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.enable_deep_supervision = False
+        self.num_epochs = 250
+
+    @staticmethod
+    def build_network_architecture(plans_manager: PlansManager,
+                                   dataset_json,
+                                   configuration_manager: ConfigurationManager,
+                                   num_input_channels,
+                                   enable_deep_supervision=False) -> nn.Module:
+        num_stages = len(configuration_manager.conv_kernel_sizes)
+
+        dim = len(configuration_manager.conv_kernel_sizes[0])
+        conv_op = convert_dim_to_conv_op(dim)
+
+        label_manager = plans_manager.get_label_manager(dataset_json)
+
+        segmentation_network_class_name = 'swin'
+        mapping = {
+            'swin': Swin
+        }
+
+        assert segmentation_network_class_name in mapping.keys(), 'The network architecture specified by the plans file ' \
+                                                                  'is non-standard (maybe your own?). Yo\'ll have to dive ' \
+                                                                  'into either this ' \
+                                                                  'function (get_network_from_plans) or ' \
+                                                                  'the init of your nnUNetModule to accommodate that.'
+        network_class = mapping[segmentation_network_class_name]
+
+        conv_or_blocks_per_stage = {
+            'n_conv_per_stage'
+            if network_class != ResidualEncoderUNet else 'n_blocks_per_stage': configuration_manager.n_conv_per_stage_encoder,
+            'n_conv_per_stage_decoder': configuration_manager.n_conv_per_stage_decoder
+        }
+        # network class name!!
+        model = network_class(
+            input_channels=num_input_channels,
+            num_classes=label_manager.num_segmentation_heads,
+        )
+        model.apply(InitWeights_He(1e-2))
+        if network_class == ResidualEncoderUNet:
+            model.apply(init_last_bn_before_add_to_0)
+        return model
+
+    def set_deep_supervision_enabled(self, enabled: bool):
+        return
+
+
+class nnUNetTrainer_swin_pre(nnUNetTrainer):
+    def __init__(
+        self,
+        plans: dict,
+        configuration: str,
+        fold: int,
+        dataset_json: dict,
+        unpack_dataset: bool = True,
+        device: torch.device = torch.device("cuda"),
+    ):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.enable_deep_supervision = False
+        self.num_epochs = 250
+
+    @staticmethod
+    def build_network_architecture(plans_manager: PlansManager,
+                                   dataset_json,
+                                   configuration_manager: ConfigurationManager,
+                                   num_input_channels,
+                                   enable_deep_supervision: bool = False) -> nn.Module:
+        num_stages = len(configuration_manager.conv_kernel_sizes)
+
+        dim = len(configuration_manager.conv_kernel_sizes[0])
+        conv_op = convert_dim_to_conv_op(dim)
+
+        label_manager = plans_manager.get_label_manager(dataset_json)
+
+        segmentation_network_class_name = 'swin'
+        mapping = {
+            'swin': Swin
+        }
+
+        assert segmentation_network_class_name in mapping.keys(), 'The network architecture specified by the plans file ' \
+                                                                  'is non-standard (maybe your own?). Yo\'ll have to dive ' \
+                                                                  'into either this ' \
+                                                                  'function (get_network_from_plans) or ' \
+                                                                  'the init of your nnUNetModule to accommodate that.'
+        network_class = mapping[segmentation_network_class_name]
+
+        conv_or_blocks_per_stage = {
+            'n_conv_per_stage'
+            if network_class != ResidualEncoderUNet else 'n_blocks_per_stage': configuration_manager.n_conv_per_stage_encoder,
+            'n_conv_per_stage_decoder': configuration_manager.n_conv_per_stage_decoder
+        }
+        # network class name!!
+        model = network_class(
+            input_channels=num_input_channels,
+            num_classes=label_manager.num_segmentation_heads,
+        )
+
+        try:
+            model_dict = torch.load("/home/linshan/VoCo/runs/logs_10k_swinv2_abdomen/current_model.pth",
+                                    map_location=torch.device('cpu'))
+            try:
+                state_dict = model_dict  # ["state_dict"]
+            except:
+                state_dict = model_dict["state_dict"]
+
+            # if mri
+            state_dict = delete_patch_embed(state_dict)
+
+            if "module." in list(state_dict.keys())[0]:
+                print("Tag 'module.' found in state dict - fixing!")
+                for key in list(state_dict.keys()):
+                    state_dict[key.replace("module.", "")] = state_dict.pop(key)
+            if "swin_vit" in list(state_dict.keys())[0]:
+                print("Tag 'swin_vit' found in state dict - fixing!")
+                for key in list(state_dict.keys()):
+                    state_dict[key.replace("swin_vit", "swinViT")] = state_dict.pop(key)
+            # We now load model weights, setting param `strict` to False, i.e.:
+            # this load the encoder weights (Swin-ViT, SSL pre-trained), but leaves
+            # the decoder weights untouched (CNN UNet decoder).
+            model.load_state_dict(state_dict, strict=False)
+            print("Using pretrained voco ema self-supervised Swin UNETR backbone weights !")
+        except ValueError:
+            raise ValueError("Self-supervised pre-trained weights not available")
+
+        return model
+
+    def set_deep_supervision_enabled(self, enabled: bool):
+        return
+
+
+def delete_patch_embed(state_dict):
+    for key in list(state_dict.keys()):
+        state_dict[key.replace("patch_embed", "bad")] = state_dict.pop(key)
+
+    return state_dict
---
+++ Finetune/nnUNet/nnunetv2/training/nnUNetTrainer/vit.py
@@ -0,0 +1,197 @@
+from typing import Union, Type, List, Tuple
+
+import torch
+from dynamic_network_architectures.building_blocks.residual_encoders import ResidualEncoder
+from dynamic_network_architectures.building_blocks.residual import BasicBlockD, BottleneckD
+from torch import nn
+from torch.nn.modules.conv import _ConvNd
+from torch.nn.modules.dropout import _DropoutNd
+
+from dynamic_network_architectures.building_blocks.plain_conv_encoder import PlainConvEncoder
+from dynamic_network_architectures.building_blocks.unet_decoder import UNetDecoder
+from dynamic_network_architectures.building_blocks.helper import convert_conv_op_to_dim
+
+import numpy as np
+from monai.networks.nets.swin_unetr import *
+from monai.networks.blocks import PatchEmbed, UnetOutBlock, UnetrBasicBlock, UnetrUpBlock
+from monai.networks.nets.swin_unetr import SwinTransformer as SwinViT
+from monai.utils import ensure_tuple_rep
+import argparse
+import torch.nn.functional as F
+
+
+class Swin(nn.Module):
+    def __init__(self, input_channels: int,
+                 num_classes: int):
+
+        super(Swin, self).__init__()
+
+        spatial_dims = 3
+        feature_size = 48
+
+        patch_size = ensure_tuple_rep(2, spatial_dims)
+        window_size = ensure_tuple_rep(7, spatial_dims)
+
+        self.swinViT = SwinViT(
+            in_chans=input_channels,
+            embed_dim=feature_size,
+            window_size=window_size,
+            patch_size=patch_size,
+            depths=[2, 2, 2, 2],
+            num_heads=[3, 6, 12, 24],
+            mlp_ratio=4.0,
+            qkv_bias=True,
+            drop_rate=0.0,
+            attn_drop_rate=0.0,
+            drop_path_rate=0.0,
+            norm_layer=torch.nn.LayerNorm,
+            use_checkpoint=True,
+            spatial_dims=spatial_dims,
+            use_v2=True,
+        )
+        norm_name = 'instance'
+        self.encoder1 = UnetrBasicBlock(
+            spatial_dims=spatial_dims,
+            in_channels=input_channels,
+            out_channels=feature_size,
+            kernel_size=3,
+            stride=1,
+            norm_name=norm_name,
+            res_block=True,
+        )
+
+        self.encoder2 = UnetrBasicBlock(
+            spatial_dims=spatial_dims,
+            in_channels=feature_size,
+            out_channels=feature_size,
+            kernel_size=3,
+            stride=1,
+            norm_name=norm_name,
+            res_block=True,
+        )
+
+        self.encoder3 = UnetrBasicBlock(
+            spatial_dims=spatial_dims,
+            in_channels=2 * feature_size,
+            out_channels=2 * feature_size,
+            kernel_size=3,
+            stride=1,
+            norm_name=norm_name,
+            res_block=True,
+        )
+
+        self.encoder4 = UnetrBasicBlock(
+            spatial_dims=spatial_dims,
+            in_channels=4 * feature_size,
+            out_channels=4 * feature_size,
+            kernel_size=3,
+            stride=1,
+            norm_name=norm_name,
+            res_block=True,
+        )
+
+        self.encoder10 = UnetrBasicBlock(
+            spatial_dims=spatial_dims,
+            in_channels=16 * feature_size,
+            out_channels=16 * feature_size,
+            kernel_size=3,
+            stride=1,
+            norm_name=norm_name,
+            res_block=True,
+        )
+
+        self.decoder5 = UnetrUpBlock(
+            spatial_dims=spatial_dims,
+            in_channels=16 * feature_size,
+            out_channels=8 * feature_size,
+            kernel_size=3,
+            upsample_kernel_size=2,
+            norm_name=norm_name,
+            res_block=True,
+        )
+
+        self.decoder4 = UnetrUpBlock(
+            spatial_dims=spatial_dims,
+            in_channels=feature_size * 8,
+            out_channels=feature_size * 4,
+            kernel_size=3,
+            upsample_kernel_size=2,
+            norm_name=norm_name,
+            res_block=True,
+        )
+
+        self.decoder3 = UnetrUpBlock(
+            spatial_dims=spatial_dims,
+            in_channels=feature_size * 4,
+            out_channels=feature_size * 2,
+            kernel_size=3,
+            upsample_kernel_size=2,
+            norm_name=norm_name,
+            res_block=True,
+        )
+        self.decoder2 = UnetrUpBlock(
+            spatial_dims=spatial_dims,
+            in_channels=feature_size * 2,
+            out_channels=feature_size,
+            kernel_size=3,
+            upsample_kernel_size=2,
+            norm_name=norm_name,
+            res_block=True,
+        )
+
+        self.decoder1 = UnetrUpBlock(
+            spatial_dims=spatial_dims,
+            in_channels=feature_size,
+            out_channels=feature_size,
+            kernel_size=3,
+            upsample_kernel_size=2,
+            norm_name=norm_name,
+            res_block=True,
+        )
+        self.out = UnetOutBlock(spatial_dims=spatial_dims, in_channels=feature_size, out_channels=num_classes)
+
+    def forward(self, x_in):
+        hidden_states_out = self.swinViT(x_in)
+
+        enc0 = self.encoder1(x_in)
+        enc1 = self.encoder2(hidden_states_out[0])
+        enc2 = self.encoder3(hidden_states_out[1])
+        enc3 = self.encoder4(hidden_states_out[2])
+        dec4 = self.encoder10(hidden_states_out[4])
+
+        dec3 = self.decoder5(dec4, hidden_states_out[3])
+        dec2 = self.decoder4(dec3, enc3)
+        dec1 = self.decoder3(dec2, enc2)
+        dec0 = self.decoder2(dec1, enc1)
+        out = self.decoder1(dec0, enc0)
+
+        return self.out(out)
+
+    def compute_conv_feature_map_size(self, input_size):
+        """
+        IMPORTANT: input_size is the input_size of the encoder!
+        :param input_size:
+        :return:
+        """
+        # first we need to compute the skip sizes. Skip bottleneck because all output feature maps of our ops will at
+        # least have the size of the skip above that (therefore -1)
+        skip_sizes = []
+        for s in range(len(self.encoder.strides) - 1):
+            skip_sizes.append([i // j for i, j in zip(input_size, self.encoder.strides[s])])
+            input_size = skip_sizes[-1]
+        # print(skip_sizes)
+
+        assert len(skip_sizes) == len(self.stages)
+
+        # our ops are the other way around, so let's match things up
+        output = np.int64(0)
+        for s in range(len(self.stages)):
+            # print(skip_sizes[-(s+1)], self.encoder.output_channels[-(s+2)])
+            # conv blocks
+            output += self.stages[s].compute_conv_feature_map_size(skip_sizes[-(s+1)])
+            # trans conv
+            output += np.prod([self.encoder.output_channels[-(s+2)], *skip_sizes[-(s+1)]], dtype=np.int64)
+            # segmentation
+            if self.deep_supervision or (s == (len(self.stages) - 1)):
+                output += np.prod([self.num_classes, *skip_sizes[-(s+1)]], dtype=np.int64)
+        return output
