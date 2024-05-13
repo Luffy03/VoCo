4462cc950bd9fbd8984ee700f1ee785f83f6a770
Linshan <52514670+Luffy03@users.noreply.github.com>
2024-05-13 16:05:38 +0800

    Delete Finetune/Atlas_test.sh

--- Finetune/Atlas_test.sh
+++
@@ -1,5 +0,0 @@
-test_data_path=./test_examples/AbdomenAtlasTest/
-save_prediction_path=./test_examples/AbdomenAtlasPredict/
-
-torchrun --master_port=21472 Atlas_test.py \
-    --test_data_path $test_data_path --save_prediction_path $save_prediction_path
