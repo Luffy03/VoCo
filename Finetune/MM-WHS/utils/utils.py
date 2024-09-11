8fc66c2a33836803c8986c24545ce9ff365b2061
Linshan <52514670+Luffy03@users.noreply.github.com>
2024-09-11 08:56:09 +0800

    Update utils.py

--- Finetune/MM-WHS/utils/utils.py
+++ Finetune/MM-WHS/utils/utils.py
@@ -12,7 +12,7 @@
 import numpy as np
 import scipy.ndimage as ndimage
 import torch
-
+import os
 
 def resample_3d(img, target_size):
     imx, imy, imz = img.shape
@@ -76,3 +76,89 @@ def distributed_all_gather(
                 gather_list = [t.cpu().numpy() for t in gather_list]
             tensor_list_out.append(gather_list)
     return tensor_list_out
+
+
+def color_map(dataset='pascal'):
+    cmap = np.zeros((256, 3), dtype='uint8')
+
+    if dataset == 'pascal' or dataset == 'coco':
+        def bitget(byteval, idx):
+            return (byteval & (1 << idx)) != 0
+
+        for i in range(256):
+            r = g = b = 0
+            c = i
+            for j in range(8):
+                r = r | (bitget(c, 0) << 7-j)
+                g = g | (bitget(c, 1) << 7-j)
+                b = b | (bitget(c, 2) << 7-j)
+                c = c >> 3
+
+            cmap[i] = np.array([r, g, b])
+
+    elif dataset == 'cityscapes':
+        cmap[0] = np.array([128, 64, 128])
+        cmap[1] = np.array([244, 35, 232])
+        cmap[2] = np.array([70, 70, 70])
+        cmap[3] = np.array([102, 102, 156])
+        cmap[4] = np.array([190, 153, 153])
+        cmap[5] = np.array([153, 153, 153])
+        cmap[6] = np.array([250, 170, 30])
+        cmap[7] = np.array([220, 220, 0])
+        cmap[8] = np.array([107, 142, 35])
+        cmap[9] = np.array([152, 251, 152])
+        cmap[10] = np.array([70, 130, 180])
+        cmap[11] = np.array([220, 20, 60])
+        cmap[12] = np.array([255,  0,  0])
+        cmap[13] = np.array([0,  0, 142])
+        cmap[14] = np.array([0,  0, 70])
+        cmap[15] = np.array([0, 60, 100])
+        cmap[16] = np.array([0, 80, 100])
+        cmap[17] = np.array([0,  0, 230])
+        cmap[18] = np.array([119, 11, 32])
+
+        cmap[19] = np.array([0, 0, 0])
+        cmap[255] = np.array([0, 0, 0])
+
+    return cmap
+
+
+def check_dir(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+
+def load(model, model_dict):
+    if "state_dict" in model_dict.keys():
+        state_dict = model_dict["state_dict"]
+    elif "network_weights" in model_dict.keys():
+        state_dict = model_dict["network_weights"]
+    elif "net" in model_dict.keys():
+        state_dict = model_dict["net"]
+    else:
+        state_dict = model_dict
+
+    if "module." in list(state_dict.keys())[0]:
+        print("Tag 'module.' found in state dict - fixing!")
+        for key in list(state_dict.keys()):
+            state_dict[key.replace("module.", "")] = state_dict.pop(key)
+
+    if "backbone." in list(state_dict.keys())[0]:
+        print("Tag 'backbone.' found in state dict - fixing!")
+    for key in list(state_dict.keys()):
+        state_dict[key.replace("backbone.", "")] = state_dict.pop(key)
+
+    if "swin_vit" in list(state_dict.keys())[0]:
+        print("Tag 'swin_vit' found in state dict - fixing!")
+        for key in list(state_dict.keys()):
+            state_dict[key.replace("swin_vit", "swinViT")] = state_dict.pop(key)
+
+    current_model_dict = model.state_dict()
+    new_state_dict = {
+        k: state_dict[k] if (k in state_dict.keys()) and (state_dict[k].size() == current_model_dict[k].size()) else current_model_dict[k]
+        for k in current_model_dict.keys()}
+
+    model.load_state_dict(new_state_dict, strict=True)
+    print("Using VoCo pretrained backbone weights !!!!!!!")
+
+    return model
